fa63dce5899af7982a1fe08ca82aaea27d21b0d4
Mobina Hoseini <mobinahoseini848@gmail.com>
2024-06-19 17:07:10 +0330

    Adjust photo of contact me

--- src/component/body-contact-me/body-contact-me.js
+++ src/component/body-contact-me/body-contact-me.js
@@ -1,5 +1,5 @@
 import "./body-contact-me.scss";
-import heroImg from "../../Assets/32-Photoroom.png";
+import heroImg from "../../Assets/32-Photoroom 1.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
