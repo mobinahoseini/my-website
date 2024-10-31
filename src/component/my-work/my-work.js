fea2018b50db19bc5f9ec70fe08a8affd852c67f
Mobina Hoseini <mobinahoseini848@gmail.com>
2024-10-31 14:48:21 +0330

    remove extra img

--- src/component/my-work/my-work.js
+++ src/component/my-work/my-work.js
@@ -1,6 +1,5 @@
 import "./my-work.scss";
 import SocialMedia from "../social-media/social-media";
-import heroImg from "../../Assets/home-img.png";
 // import Container from "react-bootstrap/Container";
 // import Row from "react-bootstrap/Row";
 // import Col from "react-bootstrap/Col";
